907a0f7f0ed17df90991118cc5c40ed35e401e4d
Malachi Dewitt <mjdewitt@usc.edu>
2025-04-30 21:55:20 -0700

    Wordle

--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,7 +14,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void backtrack(const std::string& currString, const std::string& in, const std::string& floating, std::vector<std::string>& out);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,32 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::vector<std::string> possiblePermutations;
+    std::set<std::string> out;
+    backtrack("", in, floating, possiblePermutations);
+    for (const std::string& permutation : possiblePermutations) {
+      if (dict.find(permutation) != dict.end())
+        out.emplace(permutation);
+    }
+    return out;
 }
 
 // Define any helper functions here
+void backtrack(const std::string& currString, const std::string& in, const std::string& floating, std::vector<std::string>& out) {
+  if (currString.size() == in.size()) {
+    bool hasAllFloating = true;
+    for (const char letter : floating) {
+      hasAllFloating = hasAllFloating && currString.find(letter) != std::string::npos;
+    }
+    if (hasAllFloating)
+      out.emplace_back(currString);
+  }
+  else {
+    if (in[currString.size()] != '-') {
+      backtrack(currString + in[currString.size()], in, floating, out);
+    }
+    for (char nextLetter = 'A'; nextLetter < 'Z' + 1; ++nextLetter) {
+      backtrack(currString + nextLetter, in, floating, out);
+    }
+  }
+}
